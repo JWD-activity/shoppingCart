32654c78d45f05a475a3208faeb66efcf5f7f94c
Jinok <joseo46@hotmail.com>
2021-09-27 13:09:46 +1000

    add Product component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Product from './Product';
 
-function Main() {
-  return <main>Main</main>;
+function Main({ products }) {
+  const productList = products.map((product) => (
+    <Product key={product.id} product={product}></Product>
+  ));
+
+  return (
+    <main>
+      <Col>
+        <h2>Products</h2>
+        {productList}
+      </Col>
+    </main>
+  );
 }
 
 export default Main;
