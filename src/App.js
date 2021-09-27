7fc4aa3e6c0be9c795083af9d6e1ba03cc8e635f
Jinok <joseo46@hotmail.com>
2021-09-27 12:58:35 +1000

    add Basket component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import Basket from './components/Basket';
+import Main from './components/Main';
+import Row from 'react-bootstrap/Row';
 
 function App() {
   return (
     <div className='App'>
       <Header></Header>
+      <Row>
+        <Main></Main>
+        <Basket></Basket>
+      </Row>
     </div>
   );
 }
---
+++ src/components/Basket.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+
+function Basket() {
+  return (
+    <aside>
+      <Col>
+        <h2>Cart Items</h2>
+      </Col>
+    </aside>
+  );
+}
+
+export default Basket;
