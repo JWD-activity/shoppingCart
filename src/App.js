88e2f4617e03bc929f83168954e7715c7241a158
Jinok <joseo46@hotmail.com>
2021-09-27 17:47:07 +1000

    add conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Basket from './components/Basket';
 import Main from './components/Main';
@@ -6,12 +6,14 @@ import data from './data';
 import './assets/scss/custom.scss';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
   const { products } = data;
   return (
     <div className='App'>
       <Header />
       <Main products={products}>
-        <Basket></Basket>
+        <Basket cartItems={cartItems}></Basket>
       </Main>
     </div>
   );
--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 
-function Basket() {
+function Basket({ children, cartItems }) {
   return (
     <aside>
       <Col>
         <h2 className='h4 text-primary'>Cart Items</h2>
+        {cartItems.length === 0 ? <p>Cart is empty!</p> : ''}
       </Col>
     </aside>
   );
