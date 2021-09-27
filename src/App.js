3eedfea9d2e7287a11b51712ce8b7eb9f8c8a0b1
Jinok <joseo46@hotmail.com>
2021-09-27 16:04:43 +1000

    add image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Basket from './components/Basket';
 import Main from './components/Main';
 import data from './data';
+import './assets/scss/custom.scss';
 
 function App() {
   const { products } = data;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import styles from './Product.module.css';
 
 function Product({ product }) {
   return (
     <Col>
-      <Image
-        src={product.image}
-        alt={product.name}
-        rounded
-        className={styles.img}
-      />
+      <Image src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
       <div>${product.price}</div>
-
       <Button variant='primary'>Add To Cart</Button>
     </Col>
   );
