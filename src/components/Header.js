761b6cdcb17db2e047920cd458276e889210b97b
Jinok <joseo46@hotmail.com>
2021-09-27 12:46:32 +1000

    add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
-  return <div>Hello</div>;
+  return (
+    <header>
+      <Navbar bg='light'>
+        <Container>
+          <Navbar.Brand href='#'>Small Shopping Cart</Navbar.Brand>
+          <Nav>
+            <Nav.Link href='#'>Cart</Nav.Link>
+            <Nav.Link href='#'>SignIn</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
 export default Header;
